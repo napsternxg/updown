e466a778346c34f7783457883023a7bc824f8140
vvcephei <jroesler@gmail.com>
2012-02-15 08:47:07 -0600

    working on replicating bishal's results

--- src/main/scala/updown/app/experiment/SplitExperiment.scala
+++ src/main/scala/updown/app/experiment/SplitExperiment.scala
@@ -5,15 +5,15 @@ import org.clapper.argot.ArgotParser._
 import org.clapper.argot.ArgotConverters._
 import com.weiglewilczek.slf4s.Logging
 import updown.util.Statistics
-import org.clapper.argot.{ArgotUsageException, ArgotParser}
 import updown.data.{SystemLabeledTweet, GoldLabeledTweet}
 import java.util.Arrays
+import org.clapper.argot.{MultiValueOption, ArgotUsageException, ArgotParser}
 
 abstract class SplitExperiment extends Experiment {
   // this exists purely to make the ArgotConverters appear used to IDEA
   convertByte _
-  val goldTrainSet = parser.option[String](List("G", "train"), "FILE", "gold labeled training data")
-  val goldTestSet = parser.option[String](List("g", "test"), "FILE", "gold labeled test data")
+  val goldTrainSet:MultiValueOption[String] = parser.multiOption[String](List("G", "train"), "FILE", "gold labeled training data")
+  val goldTestSet:MultiValueOption[String] = parser.multiOption[String](List("g", "test"), "FILE", "gold labeled test data")
   val targetsInputFileTest = parser.option[String](List("s", "targetsTest"), "targetsTestFile", "targets (TEST)")
 
   def doExperiment(testSet: List[GoldLabeledTweet], trainSet: List[GoldLabeledTweet]): List[SystemLabeledTweet]
@@ -23,25 +23,20 @@ abstract class SplitExperiment extends Experiment {
   def main(args: Array[String]) {
     try {
       parser.parse(args)
-      val trainFileName =
-        goldTrainSet.value match {
-          case Some(filename) => filename
-          case None => parser.usage("You must specify a gold labeled training file")
-        }
-      val testFileName =
-        goldTestSet.value match {
-          case Some(filename) => filename
-          case None => parser.usage("You must specify a gold labeled test file via")
-        }
-      val result =
-      {
-          logger.debug("starting run")
-          val result = doExperiment(TweetFeatureReader(testFileName), TweetFeatureReader(trainFileName))
-          logger.debug("ending run")
-          result
+      val trainSet: List[GoldLabeledTweet] = goldTrainSet.value.toList.flatMap((s)=>TweetFeatureReader(s))
+      val testSet: List[GoldLabeledTweet] = goldTrainSet.value.toList.flatMap((s)=>TweetFeatureReader(s))
+      if (trainSet.length == 0) {
+        parser.usage("no training instances specified")
       }
-      
-      report(trainFileName.toString+"->"+testFileName.toString ,result)
+      if (testSet.length == 0) {
+        parser.usage("no testing instances specified")
+      }
+      logger.debug("starting run")
+      val result = doExperiment(testSet, trainSet)
+      logger.debug("ending run")
+
+
+      report(goldTrainSet.value.toString + "->" + goldTestSet.value.toString, result)
       logger.debug("running cleanup code")
       System.exit(after())
     }
